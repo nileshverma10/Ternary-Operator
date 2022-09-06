94e9827dc5bcd73e7e828f3752ef5bb5f4e7de3e
Nilesh Verma <100075212+nileshverma10@users.noreply.github.com>
2022-09-06 14:58:24 +0530

    Add files via upload

---
+++ que1 (1).js
@@ -0,0 +1,6 @@
+//Problem 1 : Given an array print the position (starting with 1) and the element in a single line.
+
+let arr=["nilesh","mahesh","suresh","rajesh","kamlesh"];
+for(i=0;i<arr.length;i++){
+  console.log(i+1,arr[i]);
+}
---
+++ que3.js
@@ -0,0 +1,12 @@
+//Problem 2 : Given a character in lower case print the upper case character
+
+let char="w";
+let arr1=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+let arr2=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+for(i=0;i<=arr1.length;i++){
+    if(char==arr1[i]){
+      char=arr2[i];
+    }
+}
+ console.log(char);
